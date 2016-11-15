8cfbe849fdc7e092548657f7852544dfd55a62f8
AdamHermosa <krocka56@hotmail.com>
2016-11-15 10:32:37 -0500

    First Push
    dont fuck up my ui shit

---
+++ i HATE! my job/Assets/Scripts/MainMenuRotation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuRotation : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Time.timeScale = Time.deltaTime;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(0, transform.position.y + .05f, 0);
+    }
+}
