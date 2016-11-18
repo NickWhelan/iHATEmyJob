341d8745d619e108581114c3dd52efcc5356c6d9
Nick Whelan <nickwhelan5@gmail.com>
2016-11-18 16:06:46 -0500

    More camera work
    - Added Camera rotation
    - updated Bezier script
    - updated prefabs for camera

--- i HATE! my job/Assets/Scripts/Beziercurve.cs
+++ i HATE! my job/Assets/Scripts/Beziercurve.cs
@@ -4,19 +4,12 @@ using System.Collections;
 public class Beziercurve : MonoBehaviour
 {
     public Transform OrginPoint, EndPoint, OrginPointControl, EndPointControl;
-    Vector3 Vec3_OrginPoint, Vec3_EndPoint, Vec3_OrginPointControl, Vec3_EndPointControl;
     LineRenderer line;
     public int numberOfsegInLine = 10;
     // Update is called once per frame
     public void Start()
     {
-        Vec3_OrginPoint = OrginPoint.position;
-        Vec3_EndPoint = EndPoint.position;
-        Vec3_OrginPointControl = OrginPointControl.position;
-        Vec3_EndPointControl = EndPointControl.position;
         line = GetComponent<LineRenderer>();
-        //EndPoint.transform.position = cam.ScreenToWorldPoint(new Vector3(0, 0, 38.5f));
-        //OrginPointControlObj.transform.position = OrginPoint;
         line.SetVertexCount(numberOfsegInLine);
     }
     void Update()
@@ -27,7 +20,7 @@ public class Beziercurve : MonoBehaviour
         {
             float t = (float)i / (float)(numberOfsegInLine - 1);
             // Bezier curve function
-            Vector3 pos = Mathf.Pow((1 - t), 3) * Vec3_OrginPoint + 3 * Mathf.Pow((1 - t), 2) * t * Vec3_OrginPointControl + 3 * (1 - t) * Mathf.Pow(t, 2) * Vec3_EndPointControl + Mathf.Pow(t, 3) * Vec3_EndPoint;
+            Vector3 pos = Mathf.Pow((1 - t), 3) * OrginPoint.position + 3 * Mathf.Pow((1 - t), 2) * t * OrginPointControl.position + 3 * (1 - t) * Mathf.Pow(t, 2) * EndPointControl.position + Mathf.Pow(t, 3) * EndPoint.position;
             line.SetPosition(i, pos);
         }
 
---
+++ i HATE! my job/Assets/Scripts/Camera/SecuirtyCam.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecuirtyCam : MonoBehaviour {
+    Quaternion startingAngle;
+    public Vector3 EndingAngle;
+    public float turningSpeed =1;
+    bool Towards;
+	// Use this for initialization
+	void Start () {
+        startingAngle = transform.rotation;
+        Towards = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Towards)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(EndingAngle), turningSpeed);
+            if (transform.rotation == Quaternion.Euler(EndingAngle)) {
+                Towards = false;
+            }
+        }
+        else {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation,startingAngle, turningSpeed);
+            if (transform.rotation == startingAngle)
+            {
+                Towards = true;
+            }
+        } 
+	}
+}
