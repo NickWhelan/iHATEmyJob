172c3b945ff7f56f285c041aeab7120e8368b3b5
Nick Whelan <nickwhelan5@gmail.com>
2016-11-17 22:08:50 -0500

    Cameras
    - Added a toon shader (imported from unity assets)
    - Added camera model
    - Added tv model
    - Added a BezierCurve script
    - Added materals for the tv screens
    - Made prefabs for 3 tvs

---
+++ i HATE! my job/Assets/Scripts/Beziercurve.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Beziercurve : MonoBehaviour
+{
+    public Transform OrginPoint, EndPoint, OrginPointControl, EndPointControl;
+    Vector3 Vec3_OrginPoint, Vec3_EndPoint, Vec3_OrginPointControl, Vec3_EndPointControl;
+    LineRenderer line;
+    public int numberOfsegInLine = 10;
+    // Update is called once per frame
+    public void Start()
+    {
+        Vec3_OrginPoint = OrginPoint.position;
+        Vec3_EndPoint = EndPoint.position;
+        Vec3_OrginPointControl = OrginPointControl.position;
+        Vec3_EndPointControl = EndPointControl.position;
+        line = GetComponent<LineRenderer>();
+        //EndPoint.transform.position = cam.ScreenToWorldPoint(new Vector3(0, 0, 38.5f));
+        //OrginPointControlObj.transform.position = OrginPoint;
+        line.SetVertexCount(numberOfsegInLine);
+    }
+    void Update()
+    {
+        //OrginPointControl = OrginPointControlObj.position;
+        // EndPointControl = EndPointControlObj.position;
+        for (int i = 0; i < numberOfsegInLine; ++i)
+        {
+            float t = (float)i / (float)(numberOfsegInLine - 1);
+            // Bezier curve function
+            Vector3 pos = Mathf.Pow((1 - t), 3) * Vec3_OrginPoint + 3 * Mathf.Pow((1 - t), 2) * t * Vec3_OrginPointControl + 3 * (1 - t) * Mathf.Pow(t, 2) * Vec3_EndPointControl + Mathf.Pow(t, 3) * Vec3_EndPoint;
+            line.SetPosition(i, pos);
+        }
+
+    }
+}
